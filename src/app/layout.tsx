be1bb9aa6aa5fa741fd22ec80a6b95a89542b0d7
Evil Geniots <evilgeniots@Evils-MacBook-Pro.local>
2026-01-07 22:36:34 -0700

    Fix layout font vars (remove geistSans/geistMono)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
